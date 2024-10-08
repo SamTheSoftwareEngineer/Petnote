be5a1f716d91ec44adcf15c7c230c15d17029a12
Samaira Stewart <stewartsamaira@gmail.com>
2024-10-08 18:42:06 -0400

    Created views for each model

--- api/views.py
+++ api/views.py
@@ -3,3 +3,15 @@ from .models import CustomUser, Pet, Activity
 from .serializers import CustomUserSerializer, PetSerializer, ActivitySerializer
 
 # Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
