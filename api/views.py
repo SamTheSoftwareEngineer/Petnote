1c9cbf09afa6c9327c71cc8c8d0bfccc2f293843
Samaira Stewart <stewartsamaira@gmail.com>
2024-10-08 18:35:47 -0400

    initial commit

---
+++ api/views.py
@@ -0,0 +1,5 @@
+from rest_framework import viewsets
+from .models import CustomUser, Pet, Activity
+from .serializers import CustomUserSerializer, PetSerializer, ActivitySerializer
+
+# Create your views here.
