19ec470f022e51b9e245f43e829ac1f0d113c7c8
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-18 17:11:56 -0500

    Added login auth requirement to views

--- pets/views.py
+++ pets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Pet
 from .forms import PetForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -25,5 +26,33 @@ def add_pet(request):
     return render(request, 'pets/add_pet.html', {'form': form})
 
 def pet_detail(request, pet_id):
+    pet = get_object_or_404(Pet, id=pet_id)
+    return render(request, 'pets/pet_detail.html', {'pet': pet})
+
+def edit_pet(request, pet_id):
+    pet = get_object_or_404(Pet, id=pet_id)
+    if request.method == 'POST':
+        form = PetForm(request.POST, request.FILES, instance=pet)
+        if form.is_valid():
+            form.save()
+            return redirect('pet_detail', pet_id=pet.id)
+    else:
+        form = PetForm(instance=pet)
+    return render(request, 'pets/edit_pet.html', {'form': form, 'pet': pet})
+
+def delete_pet(request, pet_id):
+    pet = get_object_or_404(Pet, id=pet_id)
+    if request.method =="POST":
+        pet.delete()
+        return redirect('pet_list')
+    return render(request, 'pets/delete_pet.html', {'pet': pet})
+
+@login_required
+def pet_list_auth(request):
+    pets = Pet.objects.all()
+    return render(request, 'pets/pet_list.html', {'pets': pets})
+
+@login_required
+def pet_detail_auth(request, pet_id):
     pet = get_object_or_404(Pet, id=pet_id)
     return render(request, 'pets/pet_detail.html', {'pet': pet})
