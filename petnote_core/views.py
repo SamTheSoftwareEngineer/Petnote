4bd4ad5533c1912f0c458bf7f3b01915a4153aa6
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-18 17:10:59 -0500

    Created signup view for user registration

--- petnote_core/views.py
+++ petnote_core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in after sign-up
+            return redirect('pet_list')  # Redirect to the pet list page
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
