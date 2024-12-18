ceeb0678d0ea391601d955e716539a8cd53c2112
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-18 17:11:30 -0500

    Added url paths for editing and deleting a pet

--- pets/urls.py
+++ pets/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.pet_list, name='pet_list'),
     path('add/', views.add_pet, name='add_pet'),
     path('<int:pet_id>/', views.pet_detail, name='pet_detail'),
+    path('<int:pet_id>/edit/', views.edit_pet, name='edit_pet'),
+    path('<int:pet_id>/delete/', views.delete_pet, name='delete_pet'),
 ]
