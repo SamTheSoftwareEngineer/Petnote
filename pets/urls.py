b640d89963b117c2c18ab11ef112ccf539b0f10f
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-17 19:46:29 -0500

    Configured urls to add a pet and view a pet's details based on id

--- pets/urls.py
+++ pets/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.pet_list, name='pet_list'),
+    path('add/', views.add_pet, name='add_pet'),
+    path('<int:pet_id>/', views.pet_detail, name='pet_detail'),
 ]
