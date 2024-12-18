67817f598af3bda8e283ac7a4c0282dad72247e8
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-17 19:46:08 -0500

    Edited pet model to include photos and allow blank breed and birthdate fields

--- pets/models.py
+++ pets/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Pet(models.Model):
+    photo = models.ImageField(upload_to='pet_photos', blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
     age = models.IntegerField()
     weight = models.IntegerField()
-    breed = models.CharField(max_length=100)
-    birth_date = models.DateField()
+    breed = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.name
