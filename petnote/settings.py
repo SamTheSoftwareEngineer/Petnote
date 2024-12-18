89177ec0e718ab6ec47660a388bb1e93b18b77ac
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-17 19:43:53 -0500

    Added media root and media urls to generate user-uploaded photos

--- petnote/settings.py
+++ petnote/settings.py
@@ -125,6 +125,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
