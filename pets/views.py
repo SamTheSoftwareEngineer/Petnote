ea6bf80b2e6bcc69d0c8be0fd8aee5d776d8330f
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-17 19:46:49 -0500

    Created pet detail and add pet views

--- pets/views.py
+++ pets/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Pet
+from .forms import PetForm
+
 # Create your views here.
 
 def pet_list(request):
-    pets = Pet.objects.all() # get all pets 
+    pets = Pet.objects.all() # get all pets from the database
     return render(request, 'pets/pet_list.html', {'pets': pets})
+
+def add_pet(request):
+    if request.method == 'POST':
+        form = PetForm(request.POST, request.FILES)
+        # Check to see if the form is valid
+        if form.is_valid():
+            pet = form.save(commit=False)
+            # Associate the pet with the logged in user 
+            pet.user = request.user
+            pet.save()
+            # Redirect to the pet list 
+            return redirect('pet_list')
+    else:
+        form = PetForm()
+
+    return render(request, 'pets/add_pet.html', {'form': form})
+
+def pet_detail(request, pet_id):
+    pet = get_object_or_404(Pet, id=pet_id)
+    return render(request, 'pets/pet_detail.html', {'pet': pet})
