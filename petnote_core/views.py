26a6a6164d3c182608af00004930bf7ccc8ec08e
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-19 22:16:39 -0500

    Added profile view

--- petnote_core/views.py
+++ petnote_core/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
+from pets.models import Pet
+
+@login_required
+def profile_view(request):
+    profile = request.user.userprofile
+    pets = Pet.objects.filter(user=request.user).count()
+
+    # If the request method is post, meaning they are updating their profile
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=profile)
+    
+    return render(request, 'core/profile.html', {'form': form, 'pets': pets})
 
 def signup(request):
     if request.method == 'POST':
