dbada2b202f089ffa56b1ce863eee5b7fb36c6cd
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-19 22:16:13 -0500

    Added next page links to paths, added profile path

--- petnote_core/urls.py
+++ petnote_core/urls.py
@@ -3,10 +3,12 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # User profile
+    path('profile/', views.profile_view, name='profile'),
     # User registration
     path('signup/', views.signup, name='signup'),
     # Login
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', next_page='profile'), name='login'),
     # Logout
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
