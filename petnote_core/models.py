34ef2c448fe0b2bd2501a623d3c4a222fc53f8ba
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-19 22:15:00 -0500

    Added reciever function to create/update user profiles

--- petnote_core/models.py
+++ petnote_core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver   
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -9,5 +10,11 @@ class UserProfile(models.Model):
     bio = models.TextField(blank=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username}'s Profile"
+    
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
      
