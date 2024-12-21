8a1351e84489f5c2d8dedd9ad280ecc431ad2453
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-20 20:10:05 -0500

    Created view for index

--- petnote_core/views.py
+++ petnote_core/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserProfileForm
 from pets.models import Pet
 
+def index(request):
+    return render(request, 'core/index.html')
 @login_required
 def profile_view(request):
     profile = request.user.userprofile
@@ -14,11 +16,13 @@ def profile_view(request):
     if request.method == 'POST':
         form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
+            print("Form is valid, saving profile...")
             form.save()
             return redirect('profile')
     else:
         form = UserProfileForm(instance=profile)
-    
+        print(form.errors)
+
     return render(request, 'core/profile.html', {'form': form, 'pets': pets})
 
 def signup(request):
