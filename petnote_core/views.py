de1e0b4735aee812a5605ffe4db66d8fbb7c04a4
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-21 17:15:50 -0500

    Fixed an issue where the view was not passing the context of profile correctly

--- petnote_core/views.py
+++ petnote_core/views.py
@@ -7,31 +7,34 @@ from pets.models import Pet
 
 def index(request):
     return render(request, 'core/index.html')
+
 @login_required
 def profile_view(request):
     profile = request.user.userprofile
-    pets = Pet.objects.filter(user=request.user).count()
+    pets_count = Pet.objects.filter(user=request.user).count()
 
-    # If the request method is post, meaning they are updating their profile
     if request.method == 'POST':
         form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
-            print("Form is valid, saving profile...")
             form.save()
             return redirect('profile')
     else:
         form = UserProfileForm(instance=profile)
-        print(form.errors)
 
-    return render(request, 'core/profile.html', {'form': form, 'pets': pets})
+    context = {
+        'form': form,
+        'pets': pets_count,
+        'profile': profile,
+    }
+    return render(request, 'core/profile.html', context)
 
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Automatically log the user in after sign-up
-            return redirect('pet_list')  # Redirect to the pet list page
+            login(request, user)
+            return redirect('pet_list')
     else:
         form = UserCreationForm()
     return render(request, 'core/signup.html', {'form': form})
