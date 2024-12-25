8496f4c856f4d8f930f052a734b37dbc9095d69e
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-24 21:56:13 -0500

    Changed imports

--- pets/views.py
+++ pets/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Pet
+from activities.models import Activity
 from .forms import PetForm
 from django.contrib.auth.decorators import login_required
-
+from django.utils import timezone
 # Create your views here.
-
+@login_required
 def pet_list(request):
     pets = Pet.objects.filter(user=request.user) # get all associated pets from the database
     return render(request, 'pets/pet_list.html', {'pets': pets})
@@ -29,7 +30,10 @@ def add_pet(request):
 @login_required
 def pet_detail(request, pet_id):
     pet = get_object_or_404(Pet, id=pet_id)
-    return render(request, 'pets/pet_detail.html', {'pet': pet})
+    activities = Activity.objects.filter(pet=pet)  # Filter activities for this specific pet
+    
+    return render(request, 'pets/pet_detail.html', {'pet': pet, 'activities': activities})
+
 
 @login_required
 def edit_pet(request, pet_id):
