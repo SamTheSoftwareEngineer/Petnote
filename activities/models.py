07a718d73df0ac9d33c53fe3099cd0216b68c2a0
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-24 21:52:47 -0500

    Added foreign keys to user and pet models

--- activities/models.py
+++ activities/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from pets.models import Pet
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Activity(models.Model):
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name="activities")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=100)
     activity = models.CharField(max_length=100)
     date_completed = models.DateTimeField(auto_now_add=True)
-    is_complete = models.BooleanField(default=False)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.pet.name} - {self.name} ({self.date_completed})"
