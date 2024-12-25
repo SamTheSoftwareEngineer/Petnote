11b579e229376553c8b0f36333d3361172495719
Samaira Stewart <stewartsamaira@gmail.com>
2024-12-24 21:54:58 -0500

    Updated imports

--- petnote_core/views.py
+++ petnote_core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import UserProfileForm
+from .forms import UserProfileForm, UpdateUsernameForm
 from pets.models import Pet
 
 def index(request):
@@ -25,6 +25,7 @@ def profile_view(request):
         'form': form,
         'pets': pets_count,
         'profile': profile,
+        'pets_count': pets_count
     }
     return render(request, 'core/profile.html', context)
 
@@ -41,3 +42,15 @@ def signup(request):
 
 def thanks_view(request):
     return render(request, 'core/thanks.html')
+
+@login_required
+def update_username(request):
+    if request.method == 'POST':
+        form = UpdateUsernameForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()  # Save the updated username
+            return redirect('profile')  # Redirect to the user's profile page (adjust this URL name if necessary)
+    else:
+        form = UpdateUsernameForm(instance=request.user)
+
+    return render(request, 'core/update_username.html', {'form': form})
