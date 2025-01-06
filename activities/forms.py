39a7f3e30c2e2dfc5297b6a449fcb682fe39fe64
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-05 21:16:46 -0500

    Added date completed field and widget for the activity form

--- activities/forms.py
+++ activities/forms.py
@@ -16,4 +16,8 @@ class ActivityForm(forms.ModelForm):
     activity_type = forms.ChoiceField(choices=ACTIVITY_CHOICES, required=False)
     class Meta:
         model = Activity
-        fields = ['name', 'description', 'pet', 'activity_type', 'is_completed', 'notes'] 
+        fields = ['name', 'description', 'pet', 'activity_type', 'is_completed', 'notes', 'date_completed'] 
+        widgets = {
+            'date_completed': forms.DateTimeInput(attrs={'type': 'datetime-local'}),  # For date and time input
+        }
+        
