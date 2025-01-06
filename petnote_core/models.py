8ba9d962f798ea1181616a2ccaec3bc125608d3e
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-05 19:16:38 -0500

    No significant changes

--- petnote_core/models.py
+++ petnote_core/models.py
@@ -8,7 +8,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile_images', blank=True)
     bio = models.TextField(blank=True)
-
+    
     def __str__(self):
         return f"{self.user.username}'s Profile"
     
