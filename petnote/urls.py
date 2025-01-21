3196b09391153e486f42146ff8708e68be4b7b5e
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-20 22:19:11 -0500

    Added url patterns for debug toolbar if not testing

--- petnote/urls.py
+++ petnote/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,3 +27,8 @@ urlpatterns = [
     path("pets/", include("pets.urls")),
     path("activities/", include("activities.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.TESTING:
+    urlpatterns = [
+        *urlpatterns,
+    ] + debug_toolbar_urls()
