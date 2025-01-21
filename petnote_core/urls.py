f7d5391e5be5cb35749af6aec8cf3d7baaf588d4
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-20 22:19:47 -0500

    Renamed home path

--- petnote_core/urls.py
+++ petnote_core/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     # User profile
     path('profile/', views.profile_view, name='profile'),
     # User registration
@@ -16,7 +16,12 @@ urlpatterns = [
     path('thanks/', views.thanks_view, name='thanks'),
     # Update username
     path('update-username/', views.update_username, name='update_username'),
-    # Reset password 
-    # path('password_reset/', views.password_reset, name='password_reset')
+    # Reset password
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='core/password_reset.html'), name='password_reset'),
+    # Password reset done
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'), name='password_reset_done'),
+    # Password reset confirm
+    path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='core/password_reset_confirm.html'), name='password_reset_confirm'),
+    # Password reset complete
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='core/password_reset_complete.html'), name='password_reset_complete'),
 ]
-
