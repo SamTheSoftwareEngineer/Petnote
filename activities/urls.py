b3f39f968c9168cb81404f415d3e3e5b4864b73e
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-23 20:04:59 -0500

    Added url to pet activity report

--- activities/urls.py
+++ activities/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pet_id>/add/', views.add_activity, name='add_activity'), # Correct URL
+    path('<int:pet_id>/add/', views.add_activity, name='add_activity'), 
     path('edit/<int:pk>/', views.edit_activity, name='edit_activity'),
     path('delete/<int:activity_id>/', views.delete_activity, name='delete_activity'),
+    path('<int:pet_id>/report/', views.activity_report, name='activity_report'),
 ]
