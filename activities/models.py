77176b4507c2a3c1ac57ee661c92605db0e7e4cc
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-25 23:16:02 -0500

    Added activity choices to model and added activity_name and type to model

--- activities/models.py
+++ activities/models.py
@@ -4,10 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Activity(models.Model):
+    ACTIVITY_CHOICES = [
+        ("Walk", "Walk"),
+        ("Play", "Play"),
+        ("Feed", "Feed"),
+        ("Vet", "Vet"),
+        ("Groom", "Groom"),
+        ("Training", "Training"),
+        ("Medicine", "Medicine"),
+        ("Other", "Other"),
+    ]
 
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name="activities")
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    name = models.CharField(max_length=100)
+    activity_name = models.CharField(max_length=100, default="")
+    activity_type = models.CharField(max_length=100, choices=ACTIVITY_CHOICES, default='Select Activity')
     activity = models.CharField(max_length=100)
     date_completed = models.DateTimeField()
     description = models.TextField(blank=True)
