49c3b2fed3f06d025e3dfa92d793acdcbca8ec79
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-26 20:39:09 -0500

    Added tests for the activity templates

---
+++ activities/tests/test_templates.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class ActivityTemplatesTest(SimpleTestCase):
+    def test_activity_report_view(self):
+        url = reverse('activity_report', kwargs={'pet_id': 1})
+        self.assertEqual(url, '/activities/1/report/')
+    
+    def test_add_activity_view(self):
+        url = reverse('add_activity', kwargs={'pet_id': 1})
+        self.assertEqual(url, '/activities/1/add/')
+    
+    def test_edit_activity_view(self):
+        url = reverse('edit_activity', kwargs={'pk': 1})
+        self.assertEqual(url, '/activities/edit/1/')
+    
+    def test_delete_activity_view(self):
+        url = reverse('delete_activity', kwargs={'activity_id': 1})
+        self.assertEqual(url, '/activities/delete/1/')
+    
