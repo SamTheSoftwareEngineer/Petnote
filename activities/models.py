ab15c212cc8c3eab6c42efb8d2cfefabc078dd79
Samaira Stewart <stewartsamaira@gmail.com>
2025-01-26 20:38:46 -0500

    Edited string representation of model

--- activities/models.py
+++ activities/models.py
@@ -26,4 +26,4 @@ class Activity(models.Model):
     notes = models.TextField(blank=True)
     
     def __str__(self):
-        return f"{self.pet.name} - {self.name} ({self.date_completed})"
+        return f"{self.pet.name} - {self.activity_name} ({self.date_completed})"
