9f92c2c5ca5df6126113e77f9fe8a88a32137317
Tiago Fassoni <tiagofassoni@gmail.com>
2025-01-28 00:36:43 -0300

    Added a sample environment file and fixed docker-compose
    Now for the longer explanation: I fixed two problems, one in your settings, another with the order of the initialization. Please note I tried to make the smallest possible change I possibly could. I'll explain my reasoning later.
    
    First problem is the order of initialization. Sometimes the db container might start later than the web container and the web container throws an exception when it doesn't find the database host. There is no amazing solution for this problem. Mine is to use a python script that waits until postgres is ready. Another solution is a bash script (https://github.com/vishnubob/wait-for-it), a third one is to use docker's healthcheck mechanism. I don't like using docker's healthcheck because it pollutes the log.
    
    Please note I added a dependency on psycopg. Oddly enough, psycopg is "psycopg 3", so it's newer than psycopg2. The only part where psycopg is called directly is in wait-for-it, so we probably could remove the dependency on psycopg2.
    
    The second problem is that the settings.py file has some hard-coded defaults. In particular, the default database host was 127.0.0.1. That works with a local server, but breaks when you use docker compose. The database host in docker compose is the service name. I added an .env.example with the entries you should have in your .env.
    
    Managing environment variables gets confusing super fast. My tactic for a recent small project I had was to use a .env file for both postgres and python containers and rename the variables in Django's settings.py so I can use the same environment variables everywhere. Please see the example below:
    
    ```
    # Django
    # ------------------------------------------------------------------------------
    DJANGO_SECRET_KEY=my_awesome_secret_key_here
    DJANGO_DEBUG=False
    DJANGO_ALLOWED_HOSTS=.localhost,127.0.0.1,[::1]
    
    # PostgreSQL
    # ------------------------------------------------------------------------------
    POSTGRES_HOST=postgres
    POSTGRES_PORT=5432
    POSTGRES_DB=super_awesome_db
    POSTGRES_USER=dbuser
    POSTGRES_PASSWORD=some_password
    ```

---
+++ wait_for_postgres.py
@@ -0,0 +1,38 @@
+import logging
+import os
+from time import sleep, time
+
+import psycopg
+
+check_timeout = int(os.getenv("POSTGRES_CHECK_TIMEOUT", "30"))
+check_interval = int(os.getenv("POSTGRES_CHECK_INTERVAL", "1"))
+interval_unit = "second" if check_interval == 1 else "seconds"
+config = {
+    "dbname": os.getenv("DB_NAME", "postgres"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", ""),
+    "host": os.getenv("DB_HOST", "postgres")
+}
+
+start_time = time()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+def pg_isready(host, user, password, dbname):
+    while time() - start_time < check_timeout:
+        try:
+            conn = psycopg.connect(**vars())
+            logger.info("Postgres is ready! ✨ 💅")
+            conn.close()
+            return True #noqa: TRY300
+        except psycopg.OperationalError:
+            logger.info("Postgres isn't ready. Waiting for %(check_interval) %(interval_unit)...", extra={"check_interval": check_interval, "interval_unit": interval_unit})
+            sleep(check_interval)
+
+    logger.error("We could not connect to Postgres within %(check_timeout) seconds.", extra={"check_timeout": check_timeout})
+    return False
+
+
+pg_isready(**config)
