518ea8ea0a515ec87708851bfb8cf236761064d2
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-14 21:03:11 -0500

    Created custom user model and modified userprofile to reference custom user model as foreign key

--- petnote_core/models.py
+++ petnote_core/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver   
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+class CustomUser(AbstractUser):
+    pass
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     email = models.EmailField(("email address"), max_length=254, unique=True, blank=False)
     image = models.ImageField(upload_to='profile_images', blank=True)
     bio = models.TextField(blank=True)
@@ -13,9 +15,4 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
     
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
      
