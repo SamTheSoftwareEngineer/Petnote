7b327f5e4602579722ebbf21228427c9728d3832
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-14 22:02:50 -0500

    Created profile update form and edited existing custom user creation form and change form

--- petnote_core/forms.py
+++ petnote_core/forms.py
@@ -1,34 +1,30 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 
 
+User = get_user_model()
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = get_user_model()
+        model= User
         fields = (
             "email",
             "username"
         )
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = (
             "email",
             "username"
         )
 
-class UpdateUsernameForm(forms.ModelForm):
+class ProfileUpdateForm(forms.ModelForm):
+    profile_image = forms.ImageField(required=False)
+    bio = forms.CharField(widget=forms.Textarea, required=False)
+
     class Meta:
-        model = get_user_model()
-        fields = ['username']
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        if get_user_model().objects.filter(username=username).exclude(id=self.instance.id).exists():
-            raise ValidationError("This username is already taken.")
-        return username
-
+        model = User
+        fields = ["username", "bio", "profile_image"]
     
 
