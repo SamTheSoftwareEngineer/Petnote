3ea55297410f5892a890bd7e04a501603b066e89
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-14 21:08:45 -0500

    Created Custom user forms, deleted previous forms

--- petnote_core/forms.py
+++ petnote_core/forms.py
@@ -1,35 +1,32 @@
 from django import forms 
-from .models import UserProfile
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
 
-class UserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = ['username', 'email', 'password1', 'password2']
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if User.objects.filter(email=email).exists():
-            raise ValidationError("This email address is already in use.")
-        return email
-
-class UserProfileForm(forms.ModelForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = UserProfile
-        fields = ['image', 'bio']
-        widgets = {
-            'bio': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Tell us about yourself!'}),
-        }
+        model = get_user_model()
+        fields = (
+            "email",
+            "username"
+        )
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "email",
+            "username"
+        )
 
 class UpdateUsernameForm(forms.ModelForm):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username']
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username).exclude(id=self.instance.id).exists():
+        if get_user_model().objects.filter(username=username).exclude(id=self.instance.id).exists():
             raise ValidationError("This username is already taken.")
         return username
 
