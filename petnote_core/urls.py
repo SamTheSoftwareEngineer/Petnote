9e9d7512c8d37f2316b45666ac3729ec96848bd6
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-16 21:43:04 -0500

    Removed redundant urls

--- petnote_core/urls.py
+++ petnote_core/urls.py
@@ -1,27 +1,12 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     # Home page 
     path('', views.index, name='home'),
-    # User profile
     path('profile/', views.profile_view, name='profile'),
-    # Signup
-    path('signup/', views.signup, name='signup'),
-    # Login
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    # Logout
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # Thanks
     path('thanks/', views.thanks_view, name='thanks'),
-    # Update username
-    path('update-username/', views.update_username, name='update_username'),
-    # Password reset
-    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="registration/custom_password_reset_form.html"), name="password_reset"),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name="password_reset_complete"),
 ]
 
 
