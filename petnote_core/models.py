4e91d849b5946fd01bc85556c2b81c1b517273bd
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-20 21:31:15 -0500

    Added first and last name eto custom user model

--- petnote_core/models.py
+++ petnote_core/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
     profile_image = models.ImageField(upload_to='profile_images', null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
     class Meta:
