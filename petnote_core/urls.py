001a751a6fde5f32c1ad1dd1187f81114538bfea
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-20 21:31:33 -0500

    Edited urls to match django all auth url pattern

--- petnote_core/urls.py
+++ petnote_core/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Home page 
     path('', views.index, name='home'),
-    path('profile/', views.profile_view, name='profile'),
-    path('thanks/', views.thanks_view, name='thanks'),
+    path('accounts/profile/', views.profile_view, name='profile'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
 
 
