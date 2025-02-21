c383e8db1e7fd345e9da6598cb4aaa770d807096
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-20 21:30:26 -0500

    Edited custom user admin to display a list displaay and various fields

--- petnote_core/admin.py
+++ petnote_core/admin.py
@@ -2,17 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 CustomUser = get_user_model()
 
-
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['email', 'username', "is_superuser", "is_active"]
-
+    list_display = ('email', 'first_name', 'last_name', 'is_staff') 
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
