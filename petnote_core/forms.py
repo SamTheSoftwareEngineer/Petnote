e66c928d7d00f63099a85824c774d58689272ec1
Samaira Stewart <stewartsamaira@gmail.com>
2025-02-21 22:43:30 -0500

    Removed unused import

--- petnote_core/forms.py
+++ petnote_core/forms.py
@@ -1,7 +1,6 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
-from allauth.account.forms import SignupForm 
 
 User = get_user_model()
 
--- petnote_core/views.py
+++ petnote_core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserChangeForm, ProfileUpdateForm, CustomUserCreationForm
-from allauth.account.forms import SignupForm
 from django.contrib import messages
 from pets.models import Pet
 from django.contrib.auth import login, authenticate, logout
